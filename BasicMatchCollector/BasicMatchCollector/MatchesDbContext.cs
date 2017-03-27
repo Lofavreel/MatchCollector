369983aa9213d9e3fefc2938a7af28ea103c9bf1
Timon Leschinski <t.leschinski@gmail.com>
2017-03-27 19:20:44 +0200

    Code initialized; EF and Json installed and initialized

---
+++ BasicMatchCollector/BasicMatchCollector/MatchesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMatchCollector
+{
+    public class MatchesDbContext : DbContext
+    {
+        //public DbSet<Match> Matches { get; set; }
+
+        public MatchesDbContext() : base("name = MatchesDbConnection") { }
+    }
+}
